1d5ff0d34297b35643b36fa5a5c6302ec3882052
jvishwa06 <jvishu06@gmail.com>
2025-01-13 20:35:43 +0530

    batman

--- webapi/obj/Debug/net8.0/webapi.AssemblyInfo.cs
+++ webapi/obj/Debug/net8.0/webapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9249b181cdee7e032709caf5a84d57303637c79")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
